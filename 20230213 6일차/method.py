80bed0463edde196aac8f75ca12e6b11996bc4c5
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2023-02-13 12:52:02 +0900

    Add files via upload

---
+++ 20230213 6일차/method.py
@@ -0,0 +1,11 @@
+#days_of_week = ["Mon","Tue","Wed","Thur","Fri"]
+
+name = "nico"
+print(name.upper())         # 대문자 변환
+print(name.capitalize())    # 첫글자만 대문자
+print(name.startswith("n")) # n으로 시작하는지 검사
+print(name.replace("o","ㅁ")) # 바꾸기
+print(name.endswith("o"))   # o로 끝나는지 검사
+print("nico".endswith("o"))   # o로 끝나는지 검사
+
+# 이런 다양한 메소드들이 있다.
