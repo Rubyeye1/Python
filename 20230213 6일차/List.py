31e3f6fef5d7e12afe00369ff487380a02eb0839
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2023-02-13 12:51:48 +0900

    Add files via upload

---
+++ 20230213 6일차/List.py
@@ -0,0 +1,13 @@
+days_of_week = ["Mon","Tue","Wed","Thur","Fri"]
+
+print(days_of_week.count("Wed"))   # Wed가 몇개 있는지 세기
+#days_of_week.clear()    # 삭제
+#print(days_of_week) 
+#days_of_week.reverse()   # 역으로 바꾸기
+#print(days_of_week)
+#days_of_week.append("Sat") # 더하기
+#days_of_week.append("Sun")
+#days_of_week.remove("Fri") # 빼기
+print(days_of_week[2])  # 3번째 문자 뽑기
+
+
